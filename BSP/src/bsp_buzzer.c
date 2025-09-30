ec6ffafa4cc8b53fe40c54cfd02d02165ce252df
haliwt <jonewang@126.com>
2025-09-30 16:52:35 +0800

    UPDATE buzzer sound is ok.

--- BSP/src/bsp_buzzer.c
+++ BSP/src/bsp_buzzer.c
@@ -6,11 +6,32 @@
  */
 #include "bsp.h"
 
+
+static void tim3_buzzer_sound_config(void)
+{
+    LL_TIM_DisableCounter(TIM3);
+   LL_TIM_GenerateEvent_UPDATE(TIM3);
+   LL_TIM_SetAutoReload(TIM3, 249); //1/(249+1) =0.004MHZ = 4KHZ buzzer sound 
+   LL_TIM_OC_SetCompareCH4(TIM3, 125);
+   LL_TIM_CC_EnableChannel(TIM3,LL_TIM_CHANNEL_CH4);
+   LL_TIM_EnableCounter(TIM3);
+}
+
+static void tim3_stop_buzzer_sound_config(void)
+{
+   LL_TIM_DisableCounter(TIM3);
+   LL_TIM_CC_DisableChannel(TIM3,LL_TIM_CHANNEL_CH4);
+   LL_TIM_OC_SetCompareCH4(TIM3,0);
+
+
+}
+
+
 void buzzer_sound(void)
 {
-	//HAL_TIM_PWM_Start(&htim3,TIM_CHANNEL_4);
-    osDelay(20);
-   // HAL_TIM_PWM_Stop(&htim3,TIM_CHANNEL_4);
+	tim3_buzzer_sound_config();//HAL_TIM_PWM_Start(&htim3,TIM_CHANNEL_4);
+    vTaskDelay(pdMS_TO_TICKS(20));//osDelay(20);
+   tim3_stop_buzzer_sound_config();// HAL_TIM_PWM_Stop(&htim3,TIM_CHANNEL_4);
 
 
 }
